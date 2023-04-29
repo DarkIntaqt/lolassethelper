778c737d0df679be445ea4b6068886bdd5952d1a
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2023-04-29 10:52:35 +0200

    added dynamic thresholds to the config

--- lolassethelper/config.example.py
+++ lolassethelper/config.example.py
@@ -1,15 +1,27 @@
-# CONFIG
-# Please don't publish the data in this file,
-# it may contains sensitive data
+# DON'T LEAK PUBLISH THIS FILE AS IT CONTAINS SENSITIVE DATA
 
-# Make sure to rename the file to the actual config file
+############################
+# CONFIG of lolassethelper #
+############################
+
+# DON'T LEAK PUBLISH THIS FILE AS IT CONTAINS SENSITIVE DATA
+# Read how to set up this file correctly: https://github.com/DarkIntaqt/lolassethelper/blob/master/SETUP.md
 
 
 # Insert your Riot Games API key here. Required
 apiKey = "YOUR KEY GOES HERE"
 
 
+########################################
+# Config for lolassethelper.challenges #
+########################################
+#
 # Decide whether the challenges should be translated or not
 # True = Translate (~140mb); False = Do not translate (~4mb)
 # Default is False
 translateChallenges = False
+
+# Whether the grandmaster and challenger thresholds should be dynamic or fixed.
+# True = Dynamic, False = Static - Dynamic can slow down the runtime of this script as additional requests need to be made
+# Default is False
+dynamicThresholds = False
